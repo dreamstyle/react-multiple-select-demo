06c65fb2786919bbe306f72bb9f8aec93bae1eef
Nate Lee <mydreamstyle@gmail.com>
2022-10-18 13:10:24 +0800

    Add first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { CARS } from './data/car'
+import './App.css'
 
 function App() {
+  const [state, setState] = useState(CARS)
+
+  function onChange({ e, index }) {
+    const shiftKeyPressing = e.nativeEvent.shiftKey
+    const { selected } = state[index]
+    const newState = [...state]
+
+    if (shiftKeyPressing) {
+      for (let i = index; i >= 0; i--) {
+        if (newState[i].selected !== selected) break
+        newState[i].selected = !selected
+      }
+    } else {
+      newState[index].selected = !selected
+    }
+    setState(newState)
+  }
+
+  function toggleAllCheckboxes(e) {
+    const { checked } = e.target
+    setState((prevState) =>
+      prevState.map((el) => ({
+        ...el,
+        selected: checked,
+      }))
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <td>
+              <input
+                type="checkbox"
+                value={'all'}
+                onChange={toggleAllCheckboxes}
+              />
+            </td>
+            <td>編號</td>
+            <td>狀態</td>
+          </tr>
+        </thead>
+        <tbody>
+          {state.map(({ name, number, selected, disabled }, index) => (
+            <tr key={number}>
+              <td>
+                {!disabled && (
+                  <input
+                    type="checkbox"
+                    value={name}
+                    checked={selected}
+                    onChange={(e) => onChange({ e, index })}
+                  />
+                )}
+              </td>
+              <td>{number}</td>
+              <td>{name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/data/car.js
@@ -0,0 +1,48 @@
+export const CARS = [
+  {
+    number: 0,
+    selected: true,
+    name: '未派車',
+  },
+  {
+    number: 1,
+    selected: false,
+    name: '未派車',
+  },
+  {
+    number: 2,
+    selected: false,
+    name: '未派車',
+  },
+  {
+    number: 3,
+    disabled: true,
+    selected: false,
+    name: '已派車',
+  },
+  {
+    number: 4,
+    selected: false,
+    name: '未派車',
+  },
+  {
+    number: 5,
+    selected: false,
+    name: '未派車',
+  },
+  {
+    number: 6,
+    selected: true,
+    name: '未派車',
+  },
+  {
+    number: 7,
+    selected: false,
+    name: '未派車',
+  },
+  {
+    number: 8,
+    selected: false,
+    name: '未派車',
+  },
+]
